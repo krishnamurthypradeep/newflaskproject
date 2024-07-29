62098b55a9678e81255be11c02abc7a3656c9c89
krishnamurthypradeep <pradeeepmurthy@gmail.com>
2024-07-29 14:22:57 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, make_response, jsonify
+from model import db
+
+app = Flask(__name__)
+
+
+@app.route('/hello')
+def hello_world():  # put application's code here
+    return 'Hello World!'
+
+
+@app.route('/api/products', methods=['GET'])
+def getAllProducts():  # put application's code here
+    return make_response(jsonify({"products": db}), 200)
+
+# Linux on ARM (linux/arm64)
+# Linux on AMDX64 (linux/amd64)
+# windows on AMD (windows/AMD64)
+
+# Container Network Model
+
+# LibNetwork
+
+# Drivers
+
+
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ model.py
@@ -0,0 +1,9 @@
+import json
+
+
+def load_json():
+    with open("products.json") as f:
+        return json.load(f)
+
+
+db = load_json()
